3d156a492e88255f5eb55cea78a05e56a8352c56
mfaust35 <>
2019-08-20 11:23:33 -0700

    Add activity Select
    --------------------
    Add activity select with xml layout
    Modify AndroidManifest to recognize new activity
    Modify build.graddle: add anko utils, kotlin test and constraint layout

---
+++ app/src/main/java/com/faust/m/td/activities/SelectActivity.kt
@@ -0,0 +1,13 @@
+package com.faust.m.td.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.faust.m.td.R
+
+class SelectActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select)
+    }
+}
